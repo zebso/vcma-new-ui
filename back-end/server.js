af79c6da6c185993813976a71a709855eba3b1b7
zebso <zebso128@gmail.com>
2025-10-15 11:27:00 +0900

    ユーザーカメラ修正

--- back-end/server.js
+++ back-end/server.js
@@ -7,8 +7,8 @@ const http = require('http');
 const app = express();
 
 const PORT = process.env.PORT ? Number(process.env.PORT) : 3000;
-// const HOST = '10.16.246.184';
-const HOST = process.env.HOST || 'localhost';
+const HOST = '10.16.246.184';
+// const HOST = process.env.HOST || 'localhost';
 
 // ゲーム別の上限設定
 const GAME_LIMITS = {
